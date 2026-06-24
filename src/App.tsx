1f86a2cfaeab89ab7694ad1262d23ee4d312a724
leehi9817 <leehi9817@gmail.com>
2026-06-24 16:03:59 +0900

    feat: add react router example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Routes, Route, Outlet } from "react-router";
 import "./App.css";
+import IndexPage from "@/pages/IndexPage";
+import SignInPage from "@/pages/SignInPage";
+import SignUpPage from "@/pages/SignUpPage";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
