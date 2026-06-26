91c0588d88dbff0d6fdbd7bbbe0ad1a4e710a939
leehi9817 <leehi9817@gmail.com>
2026-06-26 16:10:58 +0900

    refactor: restructure count store to use actions object for state management

--- src/components/counter/controller.tsx
+++ src/components/counter/controller.tsx
@@ -2,7 +2,16 @@ import { Button } from "@/components/ui/button";
 import { useCountStore } from "@/store/count";
 
 export default function Controller() {
-  const { decrease, increase } = useCountStore();
+  // ❌ 스토어 전체를 불러오면 count 변경 시에도 버튼의 불필요한 리렌더링 발생
+  // const { increase, decrease } = useCountStore();
+
+  // ✅ 셀렉터 함수로 필요한 값만 불러오면 count 변경 시 버튼의 불필요한 리렌더링 방지
+  // const increase = useCountStore((store) => store.increase);
+  // const decrease = useCountStore((store) => store.decrease);
+
+  // ✅ actions 객체로 묶기
+  const { increase, decrease } = useCountStore((store) => store.actions);
+
   return (
     <div>
       <Button onClick={decrease}>-</Button>
