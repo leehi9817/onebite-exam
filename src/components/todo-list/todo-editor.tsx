a710c3f0e88edb285a3b9c83e72cbd4e2f467490
leehi9817 <leehi9817@gmail.com>
2026-07-03 16:43:33 +0900

    refactor: develop create todo function using useMutation

--- src/components/todo-list/todo-editor.tsx
+++ src/components/todo-list/todo-editor.tsx
@@ -1,15 +1,16 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { useCreateTodo } from "@/store/todos";
+import { useCreateTodoMutation } from "@/hooks/mutations/use-create-todo";
 
 export default function TodoEditor() {
-  const createTodo = useCreateTodo();
+  const { mutate, isPending } = useCreateTodoMutation();
   const [content, setContent] = useState("");
 
   const handleAddClick = () => {
     if (content.trim() === "") return;
-    createTodo(content);
+
+    mutate(content);
     setContent("");
   };
 
@@ -20,7 +21,9 @@ export default function TodoEditor() {
         onChange={(e) => setContent(e.target.value)}
         placeholder="새로운 할 일을 입력하세요 ..."
       />
-      <Button onClick={handleAddClick}>추가</Button>
+      <Button disabled={isPending} onClick={handleAddClick}>
+        추가
+      </Button>
     </div>
   );
 }
