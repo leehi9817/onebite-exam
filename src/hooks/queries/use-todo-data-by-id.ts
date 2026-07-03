ff2ed74ac11b5d16e70aa95721aa78f4839325c6
leehi9817 <leehi9817@gmail.com>
2026-07-03 15:39:03 +0900

    feat: add todo detail page and implement data fetching by ID

---
+++ src/hooks/queries/use-todo-data-by-id.ts
@@ -0,0 +1,17 @@
+import { fetchTodoById } from "@/api/fetch-todo-by-id";
+import { useQuery } from "@tanstack/react-query";
+
+export function useTodoDataById(id: number) {
+  return useQuery({
+    queryFn: () => fetchTodoById(id),
+    queryKey: ["todos", id],
+
+    staleTime: 1000 * 60 * 5,
+    gcTime: 5000,
+
+    // refetchOnMount: false,
+    // refetchOnWindowFocus: false,
+    // refetchOnReconnect: false,
+    // refetchInterval: false,
+  });
+}
