2d0f6f54105d862b15d5e090ffa253b665bfe721
leehi9817 <leehi9817@gmail.com>
2026-07-07 17:43:44 +0900

    refactor: change queryKey expression with constants

--- src/hooks/queries/use-todo-data-by-id.ts
+++ src/hooks/queries/use-todo-data-by-id.ts
@@ -1,12 +1,13 @@
 import { fetchTodoById } from "@/api/fetch-todo-by-id";
+import { QUERY_KEYS } from "@/lib/constants";
 import { useQuery } from "@tanstack/react-query";
 
 export function useTodoDataById(id: string) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
-    queryKey: ["todos", id],
+    queryKey: QUERY_KEYS.todo.detail(id),
 
-    staleTime: 1000 * 60 * 5,
+    staleTime: 300000,
     gcTime: 5000,
 
     // refetchOnMount: false,
