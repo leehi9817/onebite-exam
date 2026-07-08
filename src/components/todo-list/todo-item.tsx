bb7c3bbb49595565a3968183f8f3515b9074f4ff
leehi9817 <leehi9817@gmail.com>
2026-07-08 13:54:15 +0900

    feat: implement delete todo functionality with onSuccess

--- src/components/todo-list/todo-item.tsx
+++ src/components/todo-list/todo-item.tsx
@@ -1,26 +1,36 @@
 import { Button } from "@/components/ui/button";
+import { useDeleteTodoMutation } from "@/hooks/mutations/use-delete-todo-mutation";
 import { useUpdateTodoMutation } from "@/hooks/mutations/use-update-todo-mutation";
 import type { Todo } from "@/types";
 import { Link } from "react-router";
 
 export default function TodoItem({ id, content, isDone }: Todo) {
-  const { mutate } = useUpdateTodoMutation();
-  const handleDeleteClick = () => {};
+  const { mutate: deleteTodo, isPending: isDeleteTodoPending } =
+    useDeleteTodoMutation();
+  const { mutate: updateTodo } = useUpdateTodoMutation();
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
   const handleCheckboxChange = () => {
-    mutate({ id, isDone: !isDone });
+    updateTodo({ id, isDone: !isDone });
   };
 
   return (
     <div className="flex items-center justify-between border p-2">
       <div className="flex gap-5">
         <input
+          disabled={isDeleteTodoPending}
           onChange={handleCheckboxChange}
           type={"checkbox"}
           checked={isDone}
         />
         <Link to={`/todolist/${id}`}>{content}</Link>
       </div>
-      <Button onClick={handleDeleteClick} variant={"destructive"}>
+      <Button
+        disabled={isDeleteTodoPending}
+        onClick={handleDeleteClick}
+        variant={"destructive"}
+      >
         삭제
       </Button>
     </div>
